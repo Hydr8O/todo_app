092b0d86a59a898f7e3c873097f222b7342002ea
Hydr8O <bananov888@gmail.com>
2020-09-21 18:10:41 +0300

    style burger icon

--- core/static/core/js/main.js
+++ core/static/core/js/main.js
@@ -7,8 +7,9 @@ addToggleNavHandler = () => {
     navList = document.querySelector('.nav__list');
     burger.addEventListener('click', () => {
         navList.classList.toggle('hidden--nav');
+        burger.classList.toggle('change--burger');
     });
-}
+};
 
 stopTransitionOnLoad();
 addToggleNavHandler();
